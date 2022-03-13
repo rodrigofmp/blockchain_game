abde4f02666bc163f8c5935c1a0ca933c958cba4
rodrigofmp <rodrigofmp@gmail.com>
2022-03-13 19:26:50 -0300

    Smart contract with tests

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const MemoryToken = artifacts.require('./MemoryToken.sol')
 
 require('chai')
@@ -5,5 +7,67 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
+  let token
+
+  before(async() => {
+    token = await MemoryToken.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async() => {
+      const address = token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async() => {
+      const name = await token.name()
+      assert.equal(name, 'Memory Token')
+    })
+
+    it('has a symbol', async() => {
+      const symbol = await token.symbol()
+      assert.equal(symbol, 'MEMORY')
+    })
+  })
+
+  describe('token distribution', async () => {
+    let result
+    let wallet = accounts[0]
+
+    it('mints tokens', async() => {
+      await token.mint(wallet, 'https://www.token-uri.com/nft')
+
+      // It should increase the total supply
+      result = await token.totalSupply()
+      assert.equal(result.toString(), '1', 'total supply is correct')
+
+      // It increments owner balance
+      result = await token.balanceOf(wallet)
+      assert.equal(result.toString(), '1', 'balanceOf is correct')
+
+      // Token should belong to owner
+      result = await token.ownerOf('1')
+      assert.equal(result.toString(), wallet.toString(), 'ownerOf is correct')
+      result = await token.tokenOfOwnerByIndex(wallet, 0)
+
+      // Owner can see all tokens
+      let balanceOf = await token.balanceOf(wallet)
+      let tokenIds = []
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.tokenOfOwnerByIndex(wallet, i)
+        tokenIds.push(id.toString())
+      }
+      let expected = ['1']
+      assert.equal(tokenIds.toString(), expected.toString(), 'tokenIds are correct')
+
+      // Token URI correct
+      let tokenURI = await token.tokenURI('1')
+      assert.equal(tokenURI, 'https://www.token-uri.com/nft')
+    })
+
+  })
+
 })
